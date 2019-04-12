09a753bfac2b41ba15a0c709ff340a210c52dd95
ArtemSglv <stiw00@bk.ru>
2019-04-12 23:27:14 +0300

    - fix PopupSender

--- src/main/java/nc/project/NotificationSender/PopupSender.java
+++ src/main/java/nc/project/NotificationSender/PopupSender.java
@@ -1,12 +1,13 @@
 package nc.project.NotificationSender;
 
+import nc.project.model.Message;
 import nc.project.model.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PopupSender implements NotificationSender {
     @Override
-    public void send(User user) {
+    public void send(User user, Message message) {
 
     }
 }
