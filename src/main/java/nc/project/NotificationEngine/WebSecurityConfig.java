f19f1f95809a3245753fcb1b0bcb00881d8aeb47
Dmitry <naumenko.dmitry19@gmail.com>
2019-05-17 15:20:07 +0300

    Закрыл с security

--- src/main/java/nc/project/NotificationEngine/WebSecurityConfig.java
+++ src/main/java/nc/project/NotificationEngine/WebSecurityConfig.java
@@ -15,6 +15,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+            .and()
                 .httpBasic()
                 .disable();
     }
