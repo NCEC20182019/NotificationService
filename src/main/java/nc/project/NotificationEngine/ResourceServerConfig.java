ca1f605e706b924082a67d645ca8026b21e7c30b
Dmitry <naumenko.dmitry19@gmail.com>
2019-05-26 21:23:32 +0300

    Resource Server Config

--- src/main/java/nc/project/NotificationEngine/ResourceServerConfig.java
+++ src/main/java/nc/project/NotificationEngine/ResourceServerConfig.java
@@ -4,13 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
-@Order(99)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    public void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .antMatchers("/notifications/**")
